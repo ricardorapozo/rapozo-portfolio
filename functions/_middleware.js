e3311681bf403c53aa5219d115dde8bd2574a1d6
ricardorapozo <ricardorapozo@gmail.com>
2026-03-01 17:59:52 -0300

    remover  _middleware.js

--- functions/_middleware.js
+++
@@ -1,9 +0,0 @@
-export function onRequest(context) {
-  const url = new URL(context.request.url);
-
-  if (url.pathname === "/") {
-    return Response.redirect(url.origin + "/motion/", 301);
-  }
-
-  return context.next();
-}
